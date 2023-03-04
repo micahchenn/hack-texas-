2007fb48913c988ddf71af45b29c7d6df385e2ea
Adish Sundar <105994025+AdishSundar@users.noreply.github.com>
2023-03-04 16:34:00 -0600

    Update Button.py

--- Button.py
+++ Button.py
@@ -3,24 +3,41 @@ import subprocess
 
 root = tk.Tk()
 root.geometry("1500x1500")
-root.configure(bg="beige")
+
+# Set the background image to a water image
+background_image = tk.PhotoImage(file="/Users/adishsundar/Desktop/Untitled design (7).png")
+background_label = tk.Label(root, image=background_image)
+background_label.place(relx=0.5, rely=0.5, anchor="center")
+
+
+# Add a raindrop image as an overlay on top of the water image
 
 def on_button_click():
     subprocess.run(["python", "Survey.py"])
     button.configure(fg="teal")
 
-# Create a Label widget for the title
-title_label = tk.Label(root, text="Sustainability", font=("Helvetica", 50), bg="green", justify="center")
-title_label.grid(row=0, column=0, columnspan=2, padx=575, pady=250)
 
-button = tk.Button(root, text="Water Usage - Sprinkler Recommendation!", command=on_button_click, bg="#00bfff", fg="black", font=("Helvetica", 16), relief="raised", bd=5)
-button.config(height=3, width=50)
+# Create a Label widget for the title
+
+raindrop_image = tk.PhotoImage(file="/Users/adishsundar/Desktop/Screen Shot 2023-03-04 at 3.46.19 PM.png")
+
+# Scale the image to half its original size
+raindrop_image = raindrop_image.subsample(2)
+
+# Create the Button widget with the raindrop image
+button = tk.Button(root, text="Water Usage - Sprinkler Recommendation!",
+                   command=on_button_click, image=raindrop_image,
+                   font=("Helvetica", 16), relief="flat", bd=0)
+
+# Set the size of the button to match the size of the image
+button.config(width=raindrop_image.width(), height=raindrop_image.height())
 
 # Add custom styling to the button
-button.config(highlightbackground="#00bfff", highlightthickness=5)
-button.config(activebackground="#009acd", activeforeground="white")
+button.config(highlightthickness=0, highlightbackground=root.cget("bg"), highlightcolor=root.cget("bg"))
+button.config(borderwidth=0, relief="flat", bd=0)
+button.config(background="blue", foreground="white")
 
 # Position the button
-button.place(relx=0.5, rely=0.5, anchor="center")
+button.place(relx=0.5, rely=.7, anchor="center")
 
 root.mainloop()
